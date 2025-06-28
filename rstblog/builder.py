2b141cf296b840e9dfd55ec61f2ff8ae8e7b46d6
Armin Ronacher <armin.ronacher@active-4.com>
2025-06-28 15:08:52 +0200

    Remove latex support

--- rstblog/builder.py
+++ rstblog/builder.py
@@ -30,13 +30,6 @@ from pygments.lexers.php import PhpLexer
 from pygments.formatters import HtmlFormatter
 from pygments.styles import get_style_by_name
 
-# LaTeX imports
-import tempfile
-import shutil
-from hashlib import sha1
-from subprocess import Popen, PIPE
-from markupsafe import escape
-from docutils import utils
 
 from rstblog.utils import Pagination
 import yaml
@@ -473,153 +466,6 @@ class CodeBlock(Directive):
         return [nodes.raw("", formatted, format="html")]
 
 
-# === INLINED LATEX FUNCTIONALITY ===
-
-DOC_WRAPPER = r"""
-\documentclass[12pt]{article}
-\usepackage[utf8x]{inputenc}
-\usepackage{amsmath}
-\usepackage{amsthm}
-\usepackage{amssymb}
-\usepackage{amsfonts}
-%%\usepackage{mathpazo}
-\usepackage{bm}
-\usepackage[active]{preview}
-\pagestyle{empty}
-\begin{document}
-\begin{preview}
-%s
-\end{preview}
-\end{document}
-"""
-
-_depth_re = re.compile(rb"\[\d+ depth=(-?\d+)\]")
-
-
-def wrap_displaymath(math):
-    ret = []
-    for part in math.split("\n\n"):
-        ret.append("\\begin{split}%s\\end{split}\\notag" % part)
-    return "\\begin{gather}\n" + "\\\\".join(ret) + "\n\\end{gather}"
-
-
-def find_depth(stdout):
-    for line in stdout.splitlines():
-        m = _depth_re.match(line)
-        if m:
-            return int(m.group(1))
-
-
-def render_math(context, math):
-    relname = "_math/%s.png" % sha1(math.encode("utf-8")).hexdigest()
-    full_filename = context.builder.get_full_static_filename(relname)
-    url = context.builder.get_static_url(relname)
-
-    if os.path.isfile(full_filename):
-        os.remove(full_filename)
-
-    latex = DOC_WRAPPER % wrap_displaymath(math)
-
-    depth = None
-    tempdir = tempfile.mkdtemp()
-    try:
-        tf = open(os.path.join(tempdir, "math.tex"), "wb")
-        tf.write(latex.encode("utf-8"))
-        tf.close()
-
-        ltx_args = ["latex", "--interaction=nonstopmode", "math.tex"]
-
-        curdir = os.getcwd()
-        os.chdir(tempdir)
-
-        try:
-            p = Popen(ltx_args, stdout=PIPE, stderr=PIPE)
-            stdout, stderr = p.communicate()
-        finally:
-            os.chdir(curdir)
-
-        if p.returncode != 0:
-            raise Exception(
-                "latex exited with error:\n[stderr]\n%s\n"
-                "[stdout]\n%s" % (stderr, stdout)
-            )
-
-        directory = os.path.dirname(full_filename)
-        if not os.path.isdir(directory):
-            os.makedirs(directory)
-        dvipng_args = [
-            "dvipng",
-            "-o",
-            full_filename,
-            "-T",
-            "tight",
-            "-z9",
-            "-D",
-            str(int(16 * 72.27 / 10)),  # Hardcoded font_size=16
-            "-bg",
-            "Transparent",
-            "--depth",
-            os.path.join(tempdir, "math.dvi"),
-        ]
-        p = Popen(dvipng_args, stdout=PIPE, stderr=PIPE)
-        stdout, stderr = p.communicate()
-        if p.returncode != 0:
-            raise Exception(
-                "dvipng exited with error:\n[stderr]\n%s\n"
-                "[stdout]\n%s" % (stderr, stdout)
-            )
-        depth = find_depth(stdout)
-    finally:
-        try:
-            shutil.rmtree(tempdir)
-        except (IOError, OSError):
-            pass
-
-    return url, depth
-
-
-def make_imgtag(url, depth, latex):
-    bits = ['<img src="%s" alt="%s"' % (escape(url), escape(latex))]
-    if depth is not None:
-        bits.append(' style="vertical-align: %dpx"' % -depth)
-    bits.append(">")
-    return "".join(bits)
-
-
-class MathDirective(Directive):
-    has_content = True
-    required_arguments = 0
-    optional_arguments = 1
-    final_argument_whitespace = True
-    option_spec = {"label": directives.unchanged, "nowrap": directives.flag}
-
-    def run(self):
-        latex = "\n".join(self.content)
-        if self.arguments and self.arguments[0]:
-            latex = self.arguments[0] + "\n\n" + latex
-        url, _ = render_math(self.state.document.settings.rstblog_context, latex)
-        return [
-            nodes.raw(
-                "",
-                '<blockquote class="math">%s</blockquote>'
-                % make_imgtag(url, None, latex),
-                format="html",
-            )
-        ]
-
-
-def math_role(role, rawtext, text, lineno, inliner, options={}, content=[]):
-    latex = utils.unescape(text, restore_backslashes=True)
-    url, depth = render_math(inliner.document.settings.rstblog_context, latex)
-    return [
-        nodes.raw(
-            "",
-            '<span class="math">%s</span>' % make_imgtag(url, depth, latex),
-            format="html",
-        )
-    ], []
-
-
 class BuildError(ValueError):
     pass
 
@@ -679,10 +525,6 @@ class Builder(object):
         html_formatter = HtmlFormatter(style=style)
         directives.register_directive("code-block", CodeBlock)
         directives.register_directive("sourcecode", CodeBlock)
-
-        # Setup LaTeX
-        directives.register_directive("math", MathDirective)
-        roles.register_local_role("math", math_role)
 
         # Setup Blog URLs (hardcoded since no custom config)
         self.register_url("blog_index", "/", defaults={"page": 1})
