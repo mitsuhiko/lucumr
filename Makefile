eb021377a508e0ecaa06bcc08ea9e1f8d2ddb60b
Armin Ronacher <armin.ronacher@active-4.com>
2025-06-28 15:38:14 +0200

    Simplify CLI setup and builder

--- Makefile
+++ Makefile
@@ -6,10 +6,10 @@ clean:
 	rm -rf blog/_build
 
 build:
-	cd blog && uv run run-rstblog build
+	cd blog && uv run build-blog
 
 serve:
-	cd blog && uv run run-rstblog serve
+	cd blog && uv run serve-blog
 
 format:
 	uv run ruff format
---
+++ rstblog/commands.py
@@ -0,0 +1,18 @@
+import sys
+import os
+from rstblog.builder import Builder
+
+
+def main_build():
+    """Entry point for build-blog command."""
+    builder = Builder(os.getcwd())
+    builder.build()
+
+
+def main_serve():
+    """Entry point for serve-blog command."""
+    try:
+        builder = Builder(os.getcwd())
+        builder.serve(host="0.0.0.0", port=5000)
+    except KeyboardInterrupt:
+        print("\nShutting down server...")
